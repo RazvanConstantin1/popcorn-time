33145812edc407469302a0778d8115b930298533
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-09-29 13:15:05 +0200

    set up the local storage for watched movies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import ReactDOM from "react-dom/client";
+import Practice from "./components/Practice.js";
 import "./index.css";
 import App from "./App";
 
@@ -8,5 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+
+    {/* <Practice /> */}
   </React.StrictMode>
 );
